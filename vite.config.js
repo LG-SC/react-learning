f13daa5246d98c24ef4962f743ccd77a53d2bb67
Luiz Gustavo Santos da Costa <luizgustavoscosta.workmail+github@gmail.com>
2025-02-07 17:35:10 -0300

    just work man

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://lg-sc.github.io/react-learning/"
+  base: "/react-learning/"
 })
